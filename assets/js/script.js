8cb4dfda5422e72f7fbe97abaf4d9ece01bc5500
Houda BEN HAFSIA <houda.benhafsia.pro@gmail.com>
2025-10-20 17:57:34 +0200

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// Effet "machine à écrire"
+var typed = new Typed('#typed', {
+  strings: [
+    'Étudiante en IA & Data Science 💡',
+    'Passionnée par la donnée 📊',
+    'Curieuse et créative 🚀'
+  ],
+  typeSpeed: 60,
+  backSpeed: 40,
+  loop: true
+});
